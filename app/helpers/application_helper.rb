ad247b225e9481325a10feaab4193da119473c70
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-09-16 00:08:43 +0600

    upload user avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
 		elsif user.image?
 			user.image
 		else
-			ApplicationController::Base.helpers.asset_path('icon_default_avatar.png')
+			ActionController::Base.helpers.asset_path('icon_default_avatar.png')
 		end
 	end
 end
