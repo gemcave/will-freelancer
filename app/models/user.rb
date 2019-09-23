1dd43d62fa6f3784fc5ee158bf3e49df376aca5f
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-09-23 23:39:06 +0600

    order model was added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 
 	has_one_attached :avatar
 	has_many :gigs
+	has_many :buying_orders, foreign_key: "buyer_id", class_name: "Order"
+	has_many :selling_orders, foreign_key: "seller_id", class_name: "Order"
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
