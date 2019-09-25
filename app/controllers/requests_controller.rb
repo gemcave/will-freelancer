7859a940c6e421d859603f5800c437a08a27dcad
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-09-25 17:38:50 +0600

    create/edit request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,21 +3,34 @@ class RequestsController < ApplicationController
 	before_action :authenticate_user!
 	before_action :set_request, except: [:new, :create, :index, :list]
 	before_action :is_authorised, only: [:edit, :update, :destroy]
+	before_action :set_categories, only: [:new, :edit, :list]
 
 
   def index
   end
 
-  def new
+	def new
+		@request = current_user.requests.build
   end
-
+	
   def create
+		@request = current_user.requests.build(request_params)
+		if @request.save
+			redirect_to requests_path, notice: "Saved..."
+		else
+			redirect_to request.referrer, flash: {error: @request.errors.full_messages.join(', ')}
+		end
   end
 
   def edit
   end
 
-  def update
+	def update
+		if @request.update(request_params)
+			redirect_to requests_path, notice: "Saved..."
+		else
+			redirect_to request.referrer, flash: {error: @request.errors.full_messages.join(', ')}
+		end
   end
 
   def show
@@ -31,15 +44,19 @@ class RequestsController < ApplicationController
 	
 	private
 
+	def set_categories
+		@categories = Category.all
+	end
+
 	def set_request
 		@request = Request.find(params[:id])
 	end
 
-	def is_athorize
+	def is_authorised
 		redirect_to root_path, alert: "You don't have permission" unless current_user.id == @request.user_id
 	end
 
 	def request_params
-		params.require(:request).permit(:description, :category_id, :delivery_time, :budget, :attachment_file, :title)
+		params.require(:request).permit(:description, :category_id, :delivery, :budget, :attachment_file, :title)
 	end
 end
