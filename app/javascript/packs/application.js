e05acc6584a30c137513f1598d671e4a733751ac
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-09-27 17:48:46 +0600

    review view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("raty-js")
 
 window.Noty = require("noty")
 window.Dropzone = require("dropzone")
